7726430529ff25451fae1f8391b34d5640c9412f
Jeff Ligon <jeff.ligon@live.com>
2021-02-13 15:40:44 -0600

    fixed number_exp

--- the-basics/number-exp.py
+++ the-basics/number-exp.py
@@ -1,4 +1,4 @@
-number = input("Give me a number.\n")
-exp = input("Now give me an exponent for that number.\n")
+number = int(input("Give me a number.\n"))
+exp = int(input("Now give me an exponent for that number.\n"))
 result = number ** exp
 print ("Your number " + str(number) + " to the exponent of " + str(exp) + " is: " + str(result))
