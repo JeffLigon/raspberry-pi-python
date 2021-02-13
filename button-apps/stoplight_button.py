a264ddcfab0971bbd1b3616829b2762a63d37037
Jeff Ligon <jeff.ligon@live.com>
2021-02-13 15:34:11 -0600

    new folders

---
+++ button-apps/stoplight_button.py
@@ -0,0 +1,21 @@
+from gpiozero import Button, LED
+from time import time, sleep
+from random import randint
+
+red_led = LED(17)
+yellow_led = LED(27)
+green_led = LED(22)
+btn = Button(26)
+
+while True:
+    btn.wait_for_press()
+    red_led.toggle()
+    sleep(0.3)
+    yellow_led.toggle()
+    sleep(0.3)
+    green_led.toggle()
+    btn.wait_for_release()
+    sleep(3)
+    red_led.off()
+    yellow_led.off()
+    green_led.off()
