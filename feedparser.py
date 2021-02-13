0f1a89ca99c5054ad2487021fb26492aa41868fd
Jeff Ligon <jeff.ligon@live.com>
2021-02-13 15:37:18 -0600

    added wiring notes for button and LED apps

--- feedparser.py
+++
@@ -1,9 +0,0 @@
-import feedparser
-
-feed_url = "http://w1.weather.gov/xml/current_obs/KPVD.rss"
-feed = feedparser.parse(feed_url)
-
-RSSitems = feed["items"]
-for item in RSSitems:
-    weather = item["title"]
-    print (weather)
