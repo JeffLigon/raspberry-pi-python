513bc2aa695e16061226049c27d71fe704683f91
Jeff Ligon <jeff.ligon@live.com>
2021-02-15 20:12:30 -0600

    removed auth file

--- twitter-bot/auth.py
+++
@@ -1,5 +0,0 @@
-# Twitter API keys
-consumer_key        = 'PcDXVnvSNSRWg1YCAYCmSGdSI'
-consumer_secret     = 'uBr417twN5c6qmcVkNfGCpNEwYCytAbElO5OGJn232GTSJhhAz'
-access_token        = '17053961-QFtfCOnIwY05mGsb9aTi8VJWZ5fHVCSdHy36PEQSC'
-access_token_secret = 'Z3OPlM50MUaowqD7iVilF0jG8MYK6aSHOh07DW2dZvQGv'
